006b468fcd747fd38468b94e8625cd88377a7cb5
Jishnu vv <51715079+jishnu123-iete@users.noreply.github.com>
2021-01-11 11:33:00 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'jishnuvv2016@gmail.com';
+  $receiving_email_address = 'epicsinfomaticstechnology@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
